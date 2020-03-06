0fda86ed8ebbf1bef377695bd9ba640e4d2c6289
mccandlessnickolaf <56041065+mccandlessnickolaf@users.noreply.github.com>
2020-03-06 11:27:53 -0500

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return redirect("https://www.clkmg.com/nitinteotia3/zippyloan/{keyword}/{device}/{campaignid}", code=302)
+    return redirect("https://clickfunnels.com/?cf_affiliate_id=2025786&affiliate_id=2025786", code=302)
